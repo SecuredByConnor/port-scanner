8f9da7b9113808332917a484918535a7066d0632
SecuredByConnor <187035534+SecuredByConnor@users.noreply.github.com>
2025-12-04 08:58:35 +0000

    Added: user input function

--- port-scanner.py
+++ port-scanner.py
@@ -6,4 +6,6 @@ from datetime import datetime
 #Clear the screen
 subprocess.call('clear', shell=True)
 
-
+#Ask user for input
+remote_server = input("Please enter an IP address to scan: ")
+remote_server_ip = socket.gethostbyname(remote_server)
