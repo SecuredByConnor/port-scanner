c551690d355f2dc3aaec7b4ae1f24b1a5529ae74
SecuredByConnor <187035534+SecuredByConnor@users.noreply.github.com>
2025-12-04 09:51:53 +0000

    refactor: break code into functions for easier future modifications

--- port-scanner.py
+++ port-scanner.py
@@ -3,9 +3,29 @@ import subprocess
 import sys
 from datetime import datetime
 
-# Clears the terminal screen (Linux/Unix)
-subprocess.call('clear', shell=True)    # shell=True allows 'clear' command to run as if typed directly into the terminal
+def clear_terminal():
+    # Clears the terminal screen (Linux/Unix)
+    subprocess.call('clear', shell=True)    # shell=True allows 'clear' command to run as if typed directly into the terminal.
 
-# Prompt the user to enter the target IP or hostname
-remote_server = input("Please enter a hostname or IP address to scan: ")
-remote_server_ip = socket.gethostbyname(remote_server)  
+def prompt_user():
+    return input("Please enter a hostname or IP address to scan: ")
+
+def print_separator():
+    print("_" * 60)
+
+def scan_message(remote_server_ip):
+    print (f"Please wait, scanning remote host", (remote_server_ip))
+
+
+def main():
+
+    # Prompt the user to enter the target IP or hostname
+    remote_server = input("Please enter a hostname or IP address to scan: ")
+    # Resolve the hostname provided by the user to its corresponding IP address.
+    # Ensures the scan runs againest a vaild IPv4 address.
+    remote_server_ip = socket.gethostbyname(remote_server) 
+
+
+# Runs the main function
+if __name__ == "__main__":
+    main()
