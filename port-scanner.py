3a58fbc48ae9f0d3859b4f0e8548203a97964b12
SecuredByConnor <187035534+SecuredByConnor@users.noreply.github.com>
2025-12-04 09:08:48 +0000

    docs: improve code comments for clarity

--- port-scanner.py
+++ port-scanner.py
@@ -3,9 +3,9 @@ import subprocess
 import sys
 from datetime import datetime
 
-#Clear the screen
-subprocess.call('clear', shell=True)
+# Clears the terminal screen (Linux/Unix)
+subprocess.call('clear', shell=True)    # shell=True allows 'clear' command to run as if typed directly into the terminal
 
-#Ask user for input
-remote_server = input("Please enter an IP address to scan: ")
-remote_server_ip = socket.gethostbyname(remote_server)
+# Prompt the user to enter the target IP or hostname
+remote_server = input("Please enter a hostname or IP address to scan: ")
+remote_server_ip = socket.gethostbyname(remote_server)  
